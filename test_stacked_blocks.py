db6848e767839c9f0722fad2870bdf9e8e42ceb8
Daddy <michaelworden@juno.com>
2018-04-15 20:51:42 -0600

    Added stacked test (fails)

---
+++ test_stacked_blocks.py
@@ -0,0 +1,18 @@
+from drone_world.drone_world import DroneWorld
+from drone_world.planner.high_level.csa_planner import CrowSearchPlanner
+from drone_world.figure.drone_world_figure import DroneWorldFigure
+
+if __name__ == "__main__":
+    world = DroneWorld()
+    world.initialize("world_states/stacked_blocks/initial.txt")
+    DroneWorldFigure(world).show()
+
+    csa_planner = CrowSearchPlanner(world)
+    csa_planner.initialize("world_states/stacked_blocks/goal.txt")
+    csa_planner.run()
+
+    DroneWorldFigure(world).show()
+
+    drone_moves = world.get_drone_move_counter();
+
+    print "Solution provided in " + str(int(drone_moves)) + " moves."
